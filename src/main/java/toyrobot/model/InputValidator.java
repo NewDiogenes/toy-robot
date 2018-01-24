45f33437b3a2f501b4ab92988f6fef698ffa75fc
Alex Mitchem <amcodeprojects@gmail.com>
2018-01-24 17:23:27 +1100

    added validator for place commands

--- src/main/java/toyrobot/model/InputValidator.java
+++ src/main/java/toyrobot/model/InputValidator.java
@@ -1,6 +1,23 @@
 package toyrobot.model;
 
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 public interface InputValidator extends Predicate<String> {
+
+  static InputValidator place() {
+    return in -> Optional.ofNullable(in)
+        .map(i -> i.split(" "))
+        .filter(i -> i.length >= 4)
+        .filter(i -> i[0].equals("place"))
+        .filter(i -> StringUtils.isNumeric(i[1]))
+        .filter(i -> StringUtils.isNumeric(i[2]))
+        .filter(i -> Arrays.asList(new String[]{"north", "south", "east", "west"})
+            .contains(i[3].toLowerCase()))
+        .isPresent();
+  }
 }
---
+++ src/test/java/toyrobot/model/InputValidatorTest.java
@@ -0,0 +1,63 @@
+package toyrobot.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputValidatorTest {
+  private String input;
+
+  @Test
+  public void givenAValidPlaceCommandFacingNorth_place_shouldReturnTrue() {
+    input = "place 0 0 north";
+    assertTrue(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAValidPlaceCommandFacingEast_place_shouldReturnTrue() {
+    input = "place 0 0 east";
+    assertTrue(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAValidPlaceCommandFacingSouth_place_shouldReturnTrue() {
+    input = "place 0 0 south";
+    assertTrue(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAValidPlaceCommandFacingWest_place_shouldReturnTrue() {
+    input = "place 0 0 west";
+    assertTrue(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAnIncompletePlaceCommand_place_shouldReturnFalse() {
+    input = "place 0 0";
+    assertFalse(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenACommandThatIsNotPlace_place_shouldReturnFalse() {
+    input = "move";
+    assertFalse(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAXpositionThatIsNotANumber_place_shouldReturnFalse() {
+    input = "place hello";
+    assertFalse(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAYpositionThatIsNotANumber_place_shouldReturnFalse() {
+    input = "place 1 hello";
+    assertFalse(InputValidator.place().test(input));
+  }
+
+  @Test
+  public void givenAnInvalidDirection_place_shouldReturnFalse() {
+    input = "place 1 1 nowhere";
+    assertFalse(InputValidator.place().test(input));
+  }
+}
