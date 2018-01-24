00b8af02d49007b9846bbc4187f5d30448683d73
Alex Mitchem <amcodeprojects@gmail.com>
2018-01-24 16:55:01 +1100

    implemented Command.make, which accepts a validator and a parser and return a function that reads an input line and returns a RobotOperation if valid

--- src/main/java/toyrobot/model/Command.java
+++ src/main/java/toyrobot/model/Command.java
@@ -1,7 +1,13 @@
 package toyrobot.model;
 
+import java.util.Optional;
 import java.util.function.Function;
 
-public interface Command extends Function<String, RobotOperation> {
+public interface Command extends Function<String, Optional<RobotOperation>> {
 
+  static Command make(InputValidator inputValidator, InputParser inputParser){
+    return in -> Optional.ofNullable(in)
+        .filter(inputValidator)
+        .map(inputParser);
+  }
 }
---
+++ src/test/java/toyrobot/model/CommandTest.java
@@ -0,0 +1,57 @@
+package toyrobot.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class CommandTest {
+
+  @Mock
+  private InputValidator inputValidator;
+  @Mock
+  private InputParser inputParser;
+
+  private Command command;
+  private String cmd;
+
+  @Before
+  public void setUp() {
+    initMocks(this);
+    command = Command.make(inputValidator, inputParser);
+    cmd = "test";
+  }
+
+  @Test
+  public void givenANonNullInput_Command_shouldCallTheValidator() {
+    command.apply(cmd);
+    verify(inputValidator).test(eq(cmd));
+  }
+
+  @Test
+  public void givenANullInput_Command_shouldNotCallTheValidator() {
+    command.apply(null);
+    verify(inputValidator, times(0)).test(any());
+  }
+
+  @Test
+  public void givenTheValidatorReturnsFalse_Command_shouldNotCallTheParser() {
+    when(inputValidator.test(any())).thenReturn(false);
+    command.apply(cmd);
+    verify(inputParser, times(0)).apply(any());
+  }
+
+  @Test
+  public void givenTheValidatorReturnsTrue_Command_shouldCallTheParser() {
+    when(inputValidator.test(any())).thenReturn(true);
+    command.apply(cmd);
+    verify(inputParser).apply(eq(cmd));
+  }
+}
