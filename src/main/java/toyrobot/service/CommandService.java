fd79e08e0141b43601e3d2545a758dcb6c7c8930
Alex Mitchem <amcodeprojects@gmail.com>
2018-02-02 16:35:08 +1100

    refactored CommandService to use arg-less constructor, with getter/setter for commandList. Added addCommand function to CommandService

--- src/main/java/toyrobot/service/CommandService.java
+++ src/main/java/toyrobot/service/CommandService.java
@@ -2,20 +2,38 @@ package toyrobot.service;
 
 import toyrobot.domain.Command;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public class CommandService {
-  private Map<String, Command> commands;
+  private Map<String, Command> commandList;
 
-  public CommandService(Map<String, Command> commands) {
-    this.commands = commands;
+  public CommandService() {
+    this.commandList = new HashMap<>();
   }
 
   public Optional<Command> getCommand(String command) {
     return Optional.ofNullable(command)
         .map(cmd -> cmd.split(" "))
         .map(cmd -> cmd[0])
-        .map(cmd -> commands.get(cmd));
+        .map(cmd -> commandList.get(cmd));
+  }
+
+  public void addCommand(String commandName, Command command) {
+    Optional.ofNullable(commandName)
+        .filter(cmd -> Objects.nonNull(command))
+        .filter(commandList::containsKey)
+        .map(cmd -> commandList.put(cmd, command))
+        .orElseThrow(() -> new IllegalArgumentException("Unable able to add new command"));
+  }
+
+  public Map<String, Command> getCommandList() {
+    return commandList;
+  }
+
+  public void setCommandList(Map<String, Command> commandList) {
+    this.commandList = commandList;
   }
 }
--- src/test/java/toyrobot/service/CommandServiceTest.java
+++ src/test/java/toyrobot/service/CommandServiceTest.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class CommandServiceTest {
@@ -18,13 +19,14 @@ public class CommandServiceTest {
   private Command command;
   private final String input = "testInput";
   private CommandService commandService;
+  private Map<String, Command> commandList;
 
   @Before
   public void setUp() {
     initMocks(this);
-    Map<String, Command> commands = new HashMap<>();
-    commands.put(input, command);
-    commandService = new CommandService(commands);
+    commandList = new HashMap<>();
+    commandList.put(input, command);
+    commandService = new CommandService();
   }
 
   @Test
@@ -32,13 +34,40 @@ public class CommandServiceTest {
     assertEquals(Optional.empty(), commandService.getCommand(""));
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void givenServiceDoesNotAlreadyHaveCommandWithName_addCommand_shouldAddANewCommand() {
+    String commandName = "command";
+    commandService.addCommand(commandName, command);
+    assertTrue(commandService.getCommandList().containsKey(commandName));
+  }
+
   @Test
   public void givenAnInputWithACorrespondingCommand_getCommand_shouldReturnTheCommand() {
+    commandService.setCommandList(commandList);
     assertEquals(command, commandService.getCommand(input).get());
   }
 
   @Test
   public void givenAnInputWithMultipleCommands_getCommand_shouldReturnTheFirstCommand() {
+    commandService.setCommandList(commandList);
     assertEquals(command, commandService.getCommand(input + " test2").get());
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void givenCommandNameIsNull_addCommand_shouldThrowIllegalArgumentException() {
+    commandService.addCommand(null, command);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void givenCommandIsNull_addCommand_shouldThrowIllegalArgumentException() {
+    commandService.addCommand("command", null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void givenServiceAlreadyHasCommandWithName_addCommand_shouldThrowIllegalArgumentException() {
+    commandService.addCommand("command", command);
+    commandService.addCommand("command", command);
+  }
+
+
 }
